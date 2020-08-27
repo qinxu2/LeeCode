3ceb2f39b2c606f6a0bf07d1ab688c7dbf485c81
qinxu2 <13652038273@163.com>
2020-08-27 18:42:10 +0800

    delete Invalid Parenthesis

--- InvalidParenthesis/InvalidParenthesis.cpp
+++ InvalidParenthesis/InvalidParenthesis.cpp
@@ -1,10 +1,33 @@
 #include <Windows.h>
 #include <iostream>
 #include <stack>
+#include <list>
 using namespace std;
 
-void ClearContext(string& s) 
+void ClearContext(string& s)
 {
+	stack<int> st;
+	list<int> stk;
+	int len = s.size();
+	for (int i = 0; i < len; i++) {
+		if (s[i] == '(')
+			stk.push_front(i);
+		else if (s[i] == ')') {
+			if (!stk.empty())
+				stk.pop_back();
+			else 
+				s[i] = ' ';//标记下待会删掉
+		}
+	}
+	int temp;
+	while (!stk.empty()) {
+		temp = stk.front();
+		stk.pop_front();
+		s[temp] = ' ';
+	}
+}
+
+void ClearContext1(string& s) {
 	stack<int> stk;
 	int len = s.size();
 	for (int i = 0; i < len; i++) {
@@ -13,7 +36,7 @@ void ClearContext(string& s)
 		else if (s[i] == ')') {
 			if (!stk.empty())
 				stk.pop();
-			else 
+			else
 				s[i] = ' ';//标记下待会删掉
 		}
 	}
@@ -26,11 +49,15 @@ void ClearContext(string& s)
 }
 
 
-int main() 
+int main()
 {
 	string str;
+	string str1;
 	cin >> str;
 	ClearContext(str);
-	cout << str << endl;
+	cout << "list storage from    " << str << endl;
+	cin >> str1;
+	ClearContext1(str1);
+	cout << "stack storage from   " << str1 << endl;
 	return 0;
 }
